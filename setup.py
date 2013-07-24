1bded1431f23d07c1ec51bb395237f7710ef2b03
Sets88 <sets88@mail.ru>
2013-07-24 15:30:43 +0400

    add files

---
+++ setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+import vlctvstation
+
+setup(name='VLCTVStation',
+      version=vlctvstation.__version__,
+      description='Wanna build a tv station in your village? But pay nothing for software, which costs huge money, and nobody cares you have only 5 viewers, well, welcome to vlctvstation then.',
+      author='Maxim Nikitenko',
+      author_email='sets88@mail.ru',
+      packages=['vlctvstation'],
+      install_requires=[
+          'Flask', 'apscheduler'
+      ],
+      include_package_data=True,
+      zip_safe=False,
+      entry_points={
+          'console_scripts':
+              ['vlctvstation = vlctvstation.vlctvstation:main']
+        }
+     )
