31d50ede468c834076648013844d116fbf6f0c5f
Sets88 <sets88@mail.ru>
2013-09-09 16:05:52 +0400

    add modal

--- vlctvstation/static/script.js
+++ vlctvstation/static/script.js
@@ -1,4 +1,25 @@
 $(document).ready(function(){
+	$(document).on('click',".editjob,.addjob,.openmedia",function()
+	{
+		$.ajax({
+			url: $(this).attr("href") + "1/",
+			type: "GET",
+			dataType: "text",
+			data: {},
+			timeout:30000, 
+			async:false,
+			cache: false,
+			error: function(xhr)
+			{
+				console.log('Ошибка!'+xhr.status+' '+xhr.statusText);
+			},
+			success: function(text)
+			{
+				document.getElementById("myModal").innerHTML=text;
+				$("#myModal").modal();
+			}
+		});
+		return false;
 
-
+	});
 });
